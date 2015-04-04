776e06bf22856a48cc44a5ca8106fd08340965c5
Patraporn Kuhavattana <galeguy@hotmail.com>
2015-04-04 22:39:43 +0700

    Test commit

---
+++ db.js
@@ -0,0 +1,4 @@
+module.exports = {
+	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
+	'url' : 'mongodb://gerusan:lovety@proximus.modulusmongo.net:27017/zedu9hYs'
+}
